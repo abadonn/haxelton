d3b36d92e21b4d49940353deb6b95cf1a0fa1eba
Jakub Balas <abadonn@whoami.local>
2015-07-01 10:06:51 +0200

    Creating basic django structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='newzbuzz',
+    version='1.0.0',
+    description='Our project',
+    author='HaxelthonNewbies',
+    author_email='me@here.com',
+    packages=find_packages(".")
+)
