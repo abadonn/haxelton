db4984fa58d2581791da1dc76e30a292e0fe05be
Jakub Balas <abadonn@whoami.local>
2015-07-01 12:12:25 +0200

    Adding staticfile support to our project

--- newsic/settings.py
+++ newsic/settings.py
@@ -98,4 +98,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "newsic", "static"),
+)
+print(STATICFILES_DIRS)
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
