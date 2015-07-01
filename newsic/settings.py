cc384f3e05466bdf9a89ccf03cfd83e98fd67f70
Jakub Balas <abadonn@whoami.local>
2015-07-01 11:54:23 +0200

    This might even work on windows

--- newsic/settings.py
+++ newsic/settings.py
@@ -64,7 +64,7 @@ TEMPLATE_DIRS = (
     # or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, "newsic/templates"),
+    os.path.join(BASE_DIR, "newsic", "templates"),
 )
 
 WSGI_APPLICATION = 'newsic.wsgi.application'
